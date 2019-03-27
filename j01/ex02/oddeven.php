6cb87becc932ad7a167cde730626dc7fd39686b5
nicovrg <nicolasvergne88@gmail.com>
2019-03-27 16:02:43 +0100

    add ^D in ex02

--- j01/ex02/oddeven.php
+++ j01/ex02/oddeven.php
@@ -1,27 +1,25 @@
 #!/usr/bin/php
 <?php
-while (1)
+while(1)
 {
-	print("Entrez un nombre: ");
-#	if (ctrl-d == TRUE)
-#		exit();
-#	else
-#	{
-		$line = trim(fgets(STDIN));
+    print("Entrez un nombre: ");
+    $line = trim(fgets(STDIN));
+    if(feof(STDIN) == true)
+    {
+        echo "\n";
+        exit(0);
+    }
+	else
+	{
 		if (is_numeric($line) == false)
 			print("'$line' n'est pas un chiffre\n");
 		else
 		{
-			if ((int)$line % 2 == 0)
+            if (substr($line, -1) % 2 == 0)
 				print("Le chiffre $line est Pair\n");
 			else
 				print("Le chiffre $line est Impair\n");
 		}
-#	}
+	}
 }
 ?>
-
-<!-- comment exit avec ctrlD? --> 
-<!-- proteger le fait que stdin soit sur dev/null? --> 
-<!-- pourquoi quand je fais fleche up, ca remet la ligne d'avant? --> 
-<!-- et pourquoi quand je spam fleche up, ca remet remonte dans le terminal? wtf --> 

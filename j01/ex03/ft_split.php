564e9a777093ffca753a3a0ed91d0cb4ccb90e46
nicovrg <nicolasvergne88@gmail.com>
2019-03-27 16:07:32 +0100

    add aray_filter in ex03

--- j01/ex03/ft_split.php
+++ j01/ex03/ft_split.php
@@ -5,26 +5,9 @@ function ft_split($str)
 	if ($str)
 	{
 		$tab = explode (" ", $str);
+		$res = array_filter($tab, 'strlen');
 		sort ($tab);
-		return ($tab);
+		return ($res);
 	}
 }
-
-print_r(ft_split("$argv[1]"));
-
-#print_r(ft_split("Hello    World AAA"));
-#que fait la fonction si il n'y a pas de str en params? return 0?
-#comment savoir si register_argc_argv est activé?
-#pourquoi je ne peux pas parser une chaine normalement?
-
-
-#function ft_split($str)
-#{
-#	$i = 0;
-#	while($str[i])
-#	{
-#		print($str[i]);
-#		$i++;
-#	}
-#}
 ?>

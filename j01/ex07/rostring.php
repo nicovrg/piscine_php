6a1ca72ff4fef360696f8fe910b9db9933a1fb37
nicovrg <nicolasvergne88@gmail.com>
2019-03-27 19:45:52 +0100

    add ex07

---
+++ j01/ex07/rostring.php
@@ -0,0 +1,27 @@
+#!/usr/bin/php
+<?php
+function ft_split($str)
+{
+    $arr_tab = array();
+    $arr_res = array();
+	if ($str)
+	{
+		$arr_tab = preg_split('/\s+/', $str);
+		$arr_res = array_filter($arr_tab, 'strlen');
+		return ($arr_res);
+	}
+}
+if ($argc > 1)
+{
+    $arr_res = ft_split($argv[1]);
+    $word = array_shift ($arr_res);
+    array_push ($arr_res, $word);
+    foreach ($arr_res as $value)
+    {
+        $str = $str." ".$value;
+    }
+    $tmp = (count(str) - 1);
+    $str = substr($str, 1);
+    print ($str);
+}
+?>

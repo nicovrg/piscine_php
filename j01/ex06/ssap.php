06cba8bb6c41284853176a462856d657ff873a40
nicovrg <nicolasvergne88@gmail.com>
2019-03-27 22:39:27 +0100

    finish and check ex09

--- j01/ex06/ssap.php
+++ j01/ex06/ssap.php
@@ -1,19 +1,22 @@
 #!/usr/bin/php
 <?php
-if ($argc > 1)
+$index = 0;
+if ($argc == 1)
+    return ;
+foreach($argv as $i => $val)
 {
-    $j = 0;
-    foreach($argv as $i => $val)
+    if ($i != 0)
     {
-        if ($i != 0)
+        $tab = explode(" ", $val);
+        $tab = array_filter($tab, 'strlen');
+        foreach ($tab as $j => $val2)
         {
-            $tab = explode(" ", $val);
-            $tab = array_filter($tab, 'strlen');
-            $tab_tmp = $tab;
+            $tab_tmp[$index] = $val2;
+            $index++;
         }
     }
-    sort($tab_tmp);
-    foreach ($tab_tmp as $val)
-        print($val)."\n";
 }
+sort($tab_tmp);
+foreach ($tab_tmp as $val)
+    print($val)."\n";
 ?>
--- j01/ex09/ssap2.php
+++ j01/ex09/ssap2.php
@@ -1,2 +1,47 @@
 #!/usr/bin/php
-
+<?php
+function ft_split($str)
+{
+	if ($str)
+	{
+		$tab = preg_split('/\s+/', $str);
+		$res = array_filter($tab, 'strlen');
+		sort ($res);
+		return ($res);
+	}
+}
+if ($argc == 1)
+    return ;
+foreach ($argv as $key =>$value) 
+{
+    if ($value == NULL)
+        unset($argv[$key]);
+}
+$arr_all = array();
+$arr_tmp = array();
+foreach ($argv as $value)
+{
+    $arr_tmp = ft_split ($value);
+    $arr_all = array_merge($arr_all, $arr_tmp);
+}
+array_shift($arr_all);
+$arr_char = array();
+$arr_numb = array();
+$arr_else = array();
+$arr_resu = array();
+foreach ($arr_all as $val)
+{
+    if (($val[0] >= 'a' && $val[0] <= 'z') || ($val[0] >= 'A' && $val[0] <= 'Z'))
+        array_push($arr_char, $val);
+    else if ($val[0] >= '0' && $val[0] <= '1')
+        array_push($arr_numb, $val);
+    else
+        array_push($arr_else, $val);
+}
+natcasesort($arr_char);
+$arr_resu = array_merge($arr_resu, $arr_char);
+$arr_resu = array_merge($arr_resu, $arr_numb);
+$arr_resu = array_merge($arr_resu, $arr_else);
+foreach ($arr_resu as $value)
+    print ($value)."\n";
+?>

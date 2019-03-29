7c9520e9cd2dd3ca2ccea15c9c6a6f97e5e8acb4
nicovrg <nicolasvergne88@gmail.com>
2019-03-29 19:02:09 +0100

    add ex06, change img path ex05 ex06

--- j03/ex06/members_only.php
+++ j03/ex06/members_only.php
@@ -1,2 +1,15 @@
 <?php
+if ($_SERVER['PHP_AUTH_USER'] === null || $_SERVER['PHP_AUTH_PW'] === null)
+    return (0);
+else if ($_SERVER['PHP_AUTH_USER'] === "zaz" && $_SERVER['PHP_AUTH_PW'] === "jaimelespetitsponeys")
+{
+    $img = base64_encode(file_get_contents("../img/42.png"));
+    echo ("<html><body>\nBonjour Zaz<br />\n<img src='data:image/png;base64,$img'>\n</body></html>\n");
+}
+else
+{
+    header('HTTP/1.0 401 Unauthorized');
+    header("WWW-Authenticate: Basic realm=''Espace membres''");
+    echo "<html><body>Cette zone est accessible uniquement aux membres du site</body></html>\n";
+}
 ?>

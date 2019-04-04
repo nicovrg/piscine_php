5d3692443be6fbddd4b9a6c7ae36defa80b33dec
nicovrg <nicolasvergne88@gmail.com>
2019-04-04 22:16:31 +0200

    add ex05

---
+++ j07/ex05/NightsWatch.class.php
@@ -0,0 +1,16 @@
+<?PHP
+
+class NightsWatch implements IFighter
+{
+	public function recruit($fighter)
+	{
+		if (method_exists($fighter, 'fight'))
+			$fighter->fight() . "\n";
+	}
+
+	public function fight() 
+	{
+	}
+}
+
+?>
---
+++ j07/ex06/Fighter.class.php
@@ -0,0 +1,22 @@
+<?php
+
+class Fighter
+{
+	public function __construct() 
+	{
+
+	}
+
+	public function absorb ()
+	{
+		print ("(Factory absorbed a fighter of type $this->Footsoldier)\n");
+		
+	}
+
+	public function fabricate ()
+	{
+
+	}
+}
+
+?>

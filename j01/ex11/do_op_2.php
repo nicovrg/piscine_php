0d4bc7bccf7533ddd8043d897ceefda8cfec2d0c
nicovrg <nicolasvergne88@gmail.com>
2019-05-22 01:02:36 +0200

    do op 2

--- j01/ex11/do_op_2.php
+++ j01/ex11/do_op_2.php
@@ -5,13 +5,23 @@ if ($argc != 2)
 	echo ("Incorrect Parameters\n");
 	return ;
 }
-$first = trim($argv[1]);
-$operand = trim($argv[2]);
-$second = trim($argv[3]);
-if (!is_numeric($first) || !is_numeric(trim($second)))
+$i = 0;
+$clean = trim($argv[1]);
+$param = sscanf($argv[1], "%d %c %d %s");
+$first = $param[0];
+$operand = $param[1];
+$second = $param[2];
+$check = $param[3];
+if (!is_numeric($first) || !is_numeric($second) || $check)
+{
+	echo ("Syntax Error\n");
 	return ;
+}
 if ($operand !== '+' && $operand !== '-' && $operand !== '*' && $operand !== '/' && $operand !== '%')
+{
+	echo ("Syntax Error\n");
 	return ;
+}
 if ($operand === '+')
 {
 	$result = $first + $second;
@@ -32,7 +42,7 @@ if ($operand === '*')
 }
 if ($operand === '/')
 {
-	if ($second === '0')
+	if ($second === 0)
 	{
 		echo ("you're dividing by zero!\n");
 		return ;

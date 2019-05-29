14edafd6df439cfbac6987b530daf69b5811b492
nicovrg <nicolasvergne88@gmail.com>
2019-05-29 03:51:50 +0200

    method __toString makes me crazy

--- j06/ex00/Color.class.php
+++ j06/ex00/Color.class.php
@@ -1,4 +1,3 @@
-#!/usr/bin/php
 <?php
 
 Class Color 
@@ -9,34 +8,14 @@ Class Color
 	public $rgb = 0;
 	public static $verbose = false;
 	
-	function __construct ($array)
+	function check_limits ($color)
 	{
-		if ($verbose == true)
-			print ("constructor called");
-		if (array_key_exists($array['rgb'], $array) == true)
-		{
-			$this->rgb = (int)$array['rgb'];
-		}
-		else if (array_key_exists($array['red'], $array) == true && array_key_exists($array['green'], $array) == true && array_key_exists($array['blue'], $array) == true)
-		{
-			$this->red = (int)$array['red'];
-			$this->green = (int)$array['green'];
-			$this->blue = (int)$array['blue'];
-		}
-		else if ($verbose == true)
-			print ("Error\n");
-	}
-
-	function __destruct ()
-	{
-		if ($verbose == true)
-			print ("destructor called");
-		return (1);
-	}
-
-	function __toString ()
-	{
-		return('Color( red: '.sprintf("%3s", $this->red).', green: '.sprintf("%3s", $this->green).', blue: '.sprintf("%3s", $this->blue).' )');
+		if ($color < 0)
+			return (0);
+		else if ($color > 255)
+			return (255);
+		else
+			return ($color);
 	}
 
 	function convert ($input)
@@ -52,7 +31,6 @@ Class Color
 		}
 		$hex = $hex.$to_ten;
 		$array_rgb = array();
-		print $hex;
 		if (strlen($hex) == 6)
 		{
 			$val = base_convert($hex, 16, 10);
@@ -60,42 +38,78 @@ Class Color
 			$array_rgb['green'] = 255 & ($val >> 8);
 			$array_rgb['blue'] = 255 & $val;
 		}
-		else
-			return (-1);
 		return ($array_rgb);
 	}
 
+	function __construct ($array)
+	{
+		$check = 0;
+		if (array_key_exists('rgb', $array) == true)
+		{		
+			$array_rgb = $this->convert($array['rgb']);
+			$this->red = $array_rgb['red'];
+			$this->green = $array_rgb['green'];
+			$this->blue = $array_rgb['blue'];
+			$check++;
+		}
+		else if (array_key_exists('red', $array) == true && array_key_exists('green', $array) == true && array_key_exists('blue', $array) == true)
+		{
+			$this->red = round((int)$array['red'], null);
+			$this->green = round((int)$array['green'], null);
+			$this->blue = round((int)$array['blue'], null);
+			$check++;
+		}
+		$this->red = $this->check_limits($this->red);
+		$this->green = $this->check_limits($this->green);
+		$this->blue = $this->check_limits($this->blue);
+		if (self::$verbose == true && $check === 0)
+			$error = "Error\n";
+		else if (self::$verbose == true)
+			$success = "Color( red: ".printf("%3s",$this->red).", green: ".printf("%3s",$this->green).", blue: ".printf("%3s", $this->blue)." ) constructed.".PHP_EOL;
+	}
 
-	function add ()
+	function __destruct ()
 	{
-		
+		if (self::$verbose == true)
+			$success = "Color( red: ".printf("%3s",$this->red).", green: ".printf("%3s",$this->green).", blue: ".printf("%3s", $this->blue)." ) destructed.".PHP_EOL;
+		return (1);
 	}
-		
-	function sub ()
-	{
-		
-	}
-		
-	function mult ()
-	{
-		
-	}
-		
 
-	static function doc ()
+	function __toString ()
+	{
+		return ($this->success);
+	}
+
+	function doc ()
 	{
 		$file = 'Color.doc.txt';
 		$contenu = file_get_contents($file);
 		echo ($contenu."\n");
 	}
+
+	function add ($color)
+	{
+		$red = $this->red + $color->red;
+		$green = $this->green + $color->green;
+		$bleu = $this->blue + $color->blue; 
+		return (new Color(array('red' => $red, 'green' => $green, 'blue' => $blue)));
+	}
+		
+	function sub ($color)
+	{
+		$red = $this->red - $color->red;
+		$green = $this->green - $color->green;
+		$bleu = $this->blue - $color->blue; 
+		return (new Color(array('red' => $red, 'green' => $green, 'blue' => $blue)));
+	}
+
+	function mult ($color)
+	{
+		$red = $this->red * $color->red;
+		$green = $this->green * $color->green;
+		$bleu = $this->blue * $color->blue; 
+		return (new Color(array('red' => $red, 'green' => $green, 'blue' => $blue)));
+	}
 }
-
-Color::$verbose = true;
-// print( Color::doc() );
-// print_r( Color::convert(65535) );
-Color::convert(3);
-// $red = new Color( array( 'red' => 0xff, 'green' => 0   , 'blue' => 0	) );
-print( $red	 . PHP_EOL );
-
 
 ?>

b5d7fe442d29509b843a480a0fd3b1a9bd55b37e
nicovrg <nicolasvergne88@gmail.com>
2019-06-01 17:39:19 +0200

    edit

--- j03/ex06/members_only.php
+++ j03/ex06/members_only.php
@@ -1,7 +1,11 @@
 <?php
 header("WWW-Authenticate: Basic realm=''Espace membres''");
 if ($_SERVER['PHP_AUTH_USER'] === null || $_SERVER['PHP_AUTH_PW'] === null)
+{
+    header('HTTP/1.0 401 Unauthorized');
+    echo "<html><body>Cette zone est accessible uniquement aux membres du site</body></html>\n";
     return (0);
+}    
 else if ($_SERVER['PHP_AUTH_USER'] === "zaz" && $_SERVER['PHP_AUTH_PW'] === "jaimelespetitsponeys")
 {
     $img = base64_encode(file_get_contents("../img/42.png"));
